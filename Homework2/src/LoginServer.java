eaf18d0bddc1066870afb843b46601de584b04dd
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-11-22 11:51:27 +0800

    Update LoginServer.java

--- Homework2/src/LoginServer.java
+++ Homework2/src/LoginServer.java
@@ -12,21 +12,21 @@ public class LoginServer {
     Connection conn;
 
     public LoginServer() {
-        try{
+        try {
             this.conn = DBUtil.getCollection();
             this.socket = new ServerSocket(port);
             System.out.println("登录服务器启动成功");
         } catch (SQLException e) {
             System.out.println("数据库连接失败");
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("登录服务器启动失败");
             e.printStackTrace();
         }
     }
 
-    public void connect(){
-        while (true){
+    public void connect() {
+        while (true) {
             try {
                 Socket s = this.socket.accept();
                 System.out.println("客户端成功连接登录服务器");
@@ -57,7 +57,7 @@ public class LoginServer {
 }
 
 
-class ReadMessage extends Thread{
+class ReadMessage extends Thread {
     Socket s;
     Scanner in;
     PrintWriter out;
@@ -65,7 +65,7 @@ class ReadMessage extends Thread{
     public ReadMessage(Socket s, Connection conn) {
         this.s = s;
         this.conn = conn;
-        try{
+        try {
             this.in = new Scanner(s.getInputStream());
             this.out = new PrintWriter(s.getOutputStream());
         } catch (IOException e) {
@@ -74,17 +74,17 @@ class ReadMessage extends Thread{
     }
 
     @Override
-    public void run(){
-        while (true){
-            if (in.hasNext()){
+    public void run() {
+        while (true) {
+            if (in.hasNext()) {
                 String[] message = in.nextLine().split(" ");
                 User user = DBUtil.select(conn,message[1]);
-                if(message[0].equals("login")){
-                    if(user == null){
+                if (message[0].equals("login")) {
+                    if (user == null) {
                         out.println("用户名或者密码错误");
                         out.flush();
                     } else {
-                        if (user.password.equals(message[2])){
+                        if (user.password.equals(message[2])) {
                             out.println("登录成功 " + user.win + " " + user.lose + " " + user.tie);
                             out.flush();
                             break;
@@ -94,13 +94,13 @@ class ReadMessage extends Thread{
                         }
                     }
 
-                } else if(message[0].equals("register")){
-                    if(user == null){
+                } else if (message[0].equals("register")) {
+                    if (user == null) {
                         DBUtil.add(conn,message[1], message[2]);
                         out.println("注册成功");
                         out.flush();
                         break;
-                    } else{
+                    } else {
                         out.println("用户名已存在");
                         out.flush();
                     }
